7fabf37e868ce9ff261a97e482a31679b61070f1
gogi999 <g.petrovic9@gmail>
2021-03-17 11:27:04 +0100

    Added ex01/src/index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Bozo",
+    legs: 4,
+    friends: ["Milo", "Jackie"]
+};
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
